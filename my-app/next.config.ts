0ff4a9916c452e8d2a5cbec9b4ccefbe2eda022e
Matthew Quijano <mq022002@gmail.com>
2024-11-13 17:38:37 -0500

    feat: Modified to wok with sttai c sit gned

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: isProd ? "export" : undefined,
+  basePath: isProd ? "/Next.js-Crash-Course" : "",
+  assetPrefix: isProd ? "/Next.js-Crash-Course/" : "",
+  images: {
+    unoptimized: isProd,
+  },
 };
 
 export default nextConfig;
